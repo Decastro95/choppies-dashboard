5e6c992d7d9226d65f7e032a2dc0cdf69d260da3
Gustavo JrNdatipo <113350810+Decastro95@users.noreply.github.com>
2025-09-12 15:04:55 +0000

    chore: clean ESLint config for React + TS

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,36 +1,35 @@
+// .eslintrc.cjs
 module.exports = {
   root: true,
-  parser: "@typescript-eslint/parser",
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
     ecmaFeatures: {
       jsx: true,
     },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
+  plugins: ['react', 'react-hooks', '@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
   settings: {
     react: {
-      version: "detect",
+      version: 'detect',
     },
   },
-  plugins: [
-    "react",
-    "react-hooks",
-    "jsx-a11y",
-    "@typescript-eslint",
-    "prettier",
-  ],
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:jsx-a11y/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-  ],
   rules: {
-    "prettier/prettier": "error",
-    "react/react-in-jsx-scope": "off", // Not needed with React 17+
-    "@typescript-eslint/no-unused-vars": ["warn"],
+    'react/react-in-jsx-scope': 'off', // not needed with React 17+
+    'react/prop-types': 'off',        // using TS, so prop-types not needed
   },
 };
+
